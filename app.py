4a2256ea6283bcd06f1e9c27c3d603e8fc7ae7b5
ashnahussain255-jpg <ashnahussain255@gmail.com>
2026-01-16 14:21:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os, json, re
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from google import genai
+import google.generativeai as genai
 
 app = Flask(__name__)
 CORS(app)
